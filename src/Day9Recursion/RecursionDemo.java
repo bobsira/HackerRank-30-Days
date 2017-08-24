6b56653f28fa8ab40c17d89ea8e6d0aebe0b7006
bobsira <sbobfitz2@gmail.com>
2017-08-24 17:57:43 +0300

    Final updates to day 9 recursin

--- src/Day9Recursion/RecursionDemo.java
+++ src/Day9Recursion/RecursionDemo.java
@@ -4,4 +4,24 @@ package Day9Recursion;
  * Created by bobsira on 8/24/17.
  */
 public class RecursionDemo {
+
+    // The code below produces the multiple of two numbers by combining addition and recursion:
+
+    // Multiply 'n' by 'k' using addition:
+    private static int nTimesK(int n,int k){
+        System.out.println("n: " + n);
+        // Recursive case
+        if ( n > 1 ){
+            return k + nTimesK( n - 1, k);
+        }
+        // Base Case  n = 1
+        else {
+            return k;
+        }
+    }
+
+    public static void main(String[] args) {
+        int result = nTimesK(4,4);
+        System.out.println("Result: " + result);
+    }
 }
